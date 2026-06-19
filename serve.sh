1248ff9e12e72e4214389b807cb5e65456da9b77
kawaiibilli <swastik.haldar@gmail.com>
2026-06-19 07:55:43 +0530

    making the docker container survive restart

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop headpatter.xyz
 docker build -f Dockerfile . -t headpatter.xyz:v0
-docker run --env-file ./site.env --rm -it -p 80:8000 --name headpatter.xyz headpatter.xyz:v0
+docker run --env-file ./site.env --restart unless-stopped -d -p 80:8000 --name headpatter.xyz headpatter.xyz:v0
